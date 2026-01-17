1b4d750d69970bde0da36087fb66754e59228b75
Ashraful Islam <155339396+theashrafislam@users.noreply.github.com>
2026-01-18 01:01:52 +0600

    fixed vercel.json file

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ async function run() {
         app.get('/users', async (req, res) => {
             try {
                 const users = await usersCollection.find().toArray();
-                console.log(users);
+                // console.log(users);
                 res.json({
                     success: true,
                     count: users.length,
@@ -73,7 +73,7 @@ async function run() {
 
 
         // Send a ping to confirm a successful connection
-        // await client.db("admin").command({ ping: 1 });
+        await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
         // Ensures that the client will close when you finish/error
