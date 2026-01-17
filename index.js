0d5839b1a3dede6ff1d0fb3cca5154fcfbda5534
Ashraful Islam <155339396+theashrafislam@users.noreply.github.com>
2026-01-17 12:53:31 +0600

    make collection also added users

--- index.js
+++ index.js
@@ -1,6 +1,86 @@
 const express = require('express')
 const app = express()
-const port = 5000
+require("dotenv").config();
+const port = process.env.PORT || 5000;
+
+app.use(express.json());
+
+
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DATABASE_USERNAME}:${process.env.DATABASE_PASSWORD}@cluster0.gphdl2n.mongodb.net/?appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+let usersCollection
+
+async function run() {
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
+
+        const database = client.db("izibay-database");
+        usersCollection = database.collection("users");
+
+
+        app.get('/api/users', async (req, res) => {
+            try {
+                const users = await usersCollection.find().toArray();
+                console.log(users);
+                res.json({
+                    success: true,
+                    count: users.length,
+                    data: users
+                })
+            } catch (error) {
+                res.status(500).json({
+                    success: false,
+                    message: "Failed to fetch users",
+                    error: error.message
+                });
+            }
+        })
+
+        app.post("/api/users", async (req, res) => {
+            try {
+                const user = req.body;
+                const result = await usersCollection.insertOne(user);
+
+                res.json({
+                    success: true,
+                    message: "User added",
+                    insertedId: result.insertedId
+                });
+            } catch (error) {
+                res.status(500).json({
+                    success: false,
+                    message: "Failed to add user",
+                    error: error.message
+                });
+            }
+        });
+
+
+
+
+
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
 
 // ROOT API
 app.get("/", (req, res) => {
